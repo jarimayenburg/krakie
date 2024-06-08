f24757c1a6efb1eb4f872cb66c870e0998029255
Jari Maijenburg <jari.maijenburg@formelio.nl>
2024-06-08 18:04:32 +0200

    Game basics

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,33 @@
-fn main() {}
+use ggez::event::{self, EventHandler};
+use ggez::graphics::Color;
+use ggez::{graphics, Context, ContextBuilder};
+
+struct Game {}
+
+impl Game {
+    pub fn new(_ctx: &mut Context) -> Game {
+        Game {}
+    }
+}
+
+impl EventHandler for Game {
+    fn update(&mut self, _ctx: &mut Context) -> Result<(), ggez::GameError> {
+        Ok(())
+    }
+
+    fn draw(&mut self, ctx: &mut Context) -> Result<(), ggez::GameError> {
+        let canvas = graphics::Canvas::from_frame(ctx, Color::WHITE);
+
+        canvas.finish(ctx)
+    }
+}
+
+fn main() {
+    let (mut ctx, event_loop) = ContextBuilder::new("krakie", "Rico Bakker & Jari Maijenburg")
+        .build()
+        .expect("could not create ggez context!");
+
+    let game = Game::new(&mut ctx);
+
+    event::run(ctx, event_loop, game);
+}
